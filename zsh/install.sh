1a0a539c9603660be8504d76348415144fdfa802
Nihar Gavva <nihargavva@gmail.com>
2026-08-25 13:39:50 -0400

    Revert "fix: replicate fish's Ctrl+V clipboard paste in zsh"
    This reverts commit 138c1f0548f01585c4d8809cfbbd0418b52d3633.

--- zsh/install.sh
+++ zsh/install.sh
@@ -39,9 +39,9 @@ pkg_installed() {
 
 # ── prerequisites ───────────────────────────────────────────────────────────
 case $(os) in
-    debian) PREREQS=(unzip curl git fontconfig libatomic1 wl-clipboard xclip) ;;
-    fedora) PREREQS=(unzip curl git fontconfig wl-clipboard xclip) ;;
-    arch)   PREREQS=(unzip curl git fontconfig wl-clipboard xclip) ;;
+    debian) PREREQS=(unzip curl git fontconfig libatomic1) ;;
+    fedora) PREREQS=(unzip curl git fontconfig) ;;
+    arch)   PREREQS=(unzip curl git fontconfig) ;;
     macos)  PREREQS=() ;;
 esac
 
